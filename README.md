cc255df30ecb6453a95fded0e4ff558da0639caf
mackenia <mackenia@163.com>
2023-09-03 00:04:20 +0800

    update new script and update readme

--- README.md
+++ README.md
@@ -1,30 +1,30 @@
 # CQNU Helper
 
-This repository consist of a series of tools that help you enjoy CQNU life better.
+This repository consists of a series of tools that help you enjoy CQNU life better.
 
 ## LoginIntoCqnu.py
 
-You can use this script to login to your campus network or use it to automatic jobs.
+You can use this script to log in to your campus network or use it for automatic jobs.
 
 features:
 
 - Ignore login type
 - Add as automated tasks
 
-You can add some python code and made it to connect network when your computer start up.
+You can add some Python code and make it to connect the network when your computer starts up.
 
 [Binary In Rust by @mobeicanyue](https://github.com/mobeicanyue/Campus-Network-Master-Rust)
 
 ## LoginIntoCqnu.sh
 
-You can use this script to login to your campus network or use it to automatic jobs.
+You can use this script to log in to your campus network or use it for automatic jobs.
 
 features:
 
 - Ignore login type
 - Add as automated tasks
 
-You can login without python in tiny linux system but you still need `curl` and `sed` in your system.
+You can log in without Python in a tiny Linux system but you still need `curl` and `sed` in your system.
 
 ## Ms_book.py
 
@@ -32,31 +32,43 @@ You can login without python in tiny linux system but you still need `curl` and 
 
 ⚠️ Only work with Python < 3.11
 
-This script can help you book the dream rooms located at library without limitation such as banned by the official system.
+This script can help you book a dream hall seat located at the library without limitations such as being banned by the official system.
+This script includes these features:
 
-This script include these features:
-
-- book a table after three days later
+- Book a table after three days later
 - overlook system ban
-- add to you automatic system
-- reserve discontinuous time periods
-- generate .ics and send to your phone
+- add to your automatic system
+- Reserve discontinuous time periods
+- generate .ics and send it to your phone
 - auto login in
 
 bugs:
 
-- Canceled orders won't show on website but it actually canceled from backend.
+- Canceled orders won't show on the website but it actually canceled from the backend.
 
-## estimate.js
+## uBills.py
 
-Help you to fill all the annoying option boxes when evaluate the courses.
+This script is used to acquire utility bills for your dormitory.
 
 Usage:
 
-1. Open Teaching evaluate website on academic affairs system.
+```python
+from uBills import uBills
+
+ub = uBills()
+ub.ac("清风A", "2017")
+```
+
+## estimate.js
+
+Help you to fill all the annoying option boxes when evaluating the courses.
+
+Usage:
+
+1. Open Teaching evaluate website on the academic affairs system.
 2. Expand the courses list to show all courses you have this term
-3. Open webconsole through F12
-4. Paste following code
+3. Open console through F12
+4. Paste the following code
 
 Option One:
 
@@ -68,5 +80,5 @@ estimate();
 Option Two:
 
 1. Copy all code in `estimate.js`
-2. Paste it in console
+2. Paste it into the console
 3. run `estimate()`
---
+++ uBills.py
@@ -0,0 +1,214 @@
+import re
+import requests
+import json
+
+class uBills:
+
+    def __init__(self) -> None:
+        self.tsm = "http://yktfwdt.cqnu.edu.cn:8029/web/Common/Tsm.html"
+
+        self.cheat_sheet_pro = {
+            "嘉风": {
+                "head": "01",
+                "building_id": "39",
+                "aid": "0030000000002503",
+                "area": "大学城校区",
+                "building": "01嘉风苑"
+            },
+            "朗风": {
+                "head": "02",
+                "building_id": "26",
+                "aid": "0030000000002503",
+                "area": "大学城校区",
+                "building": "02朗风苑"
+            },
+             "留学生": {
+                "head": "03",
+                "building_id": "1",
+                "aid": "0030000000002503",
+                "area": "大学城校区",
+                "building": "03留学生"
+            },
+            "培训楼": {
+                "head": "04",
+                "building_id": "65",
+                "aid": "0030000000002503",
+                "area": "大学城校区",
+                "building": "04培训楼"
+            },
+            "畅风": {
+                "head": "05",
+                "building_id": "52",
+                "aid": "0030000000002503",
+                "area": "大学城校区",
+                "building": "05畅风苑"
+            },
+            "硕博楼": {
+                "head": "",
+                "building_id": "16",
+                "aid": "0030000000006701",
+                "area": "",
+                "building": "硕博楼"
+            },
+            "和风B": {
+                "head": "",
+                "building_id": "12",
+                "aid": "0030000000006701",
+                "area": "",
+                "building": "和风苑B"
+            },
+            "和风C": {
+                "head": "",
+                "building_id": "13",
+                "aid": "0030000000006701",
+                "area": "",
+                "building": "和风苑C"
+            },
+            "和风D": {
+                "head": "",
+                "building_id": "14",
+                "aid": "0030000000006701",
+                "area": "",
+                "building": "和风苑D"
+            },
+            "和风E": {
+                "head": "",
+                "building_id": "15",
+                "aid": "0030000000006701",
+                "area": "",
+                "building": "和风苑E"
+            },
+            "惠风A": {
+                "head": "",
+                "building_id": "5",
+                "aid": "0030000000006701",
+                "area": "",
+                "building": "惠风苑A"
+            },
+            "惠风B": {
+                "head": "",
+                "building_id": "6",
+                "aid": "0030000000006701",
+                "area": "",
+                "building": "惠风苑B"
+            },
+            "惠风C": {
+                "head": "",
+                "building_id": "8",
+                "aid": "0030000000006701",
+                "area": "",
+                "building": "惠风苑C"
+            },
+            "雅风A": {
+                "head": "",
+                "building_id": "9",
+                "aid": "0030000000006701",
+                "area": "",
+                "building": "雅风苑A"
+            },
+            "雅风B": {
+                "head": "",
+                "building_id": "10",
+                "aid": "0030000000006701",
+                "area": "",
+                "building": "雅风苑B"
+            },
+            "雅风C": {
+                "head": "",
+                "building_id": "11",
+                "aid": "0030000000006701",
+                "area": "",
+                "building": "雅风苑C"
+            },
+            "清风A": {
+                "head": "00100",
+                "building_id": "001",
+                "aid": "0030000000007801",
+                "area": "",
+                "building": "清风苑A栋"
+            },
+            "清风B": {
+                "head": "00200",
+                "building_id": "002",
+                "aid": "0030000000007801",
+                "area": "",
+                "building": "清风苑B栋"
+            },
+            "清风C": {
+                "head": "00300",
+                "building_id": "003",
+                "aid": "0030000000007801",
+                "area": "",
+                "building": "清风苑C栋"
+            },
+            "嘉风B": {
+                "head": "00400",
+                "building_id": "004",
+                "aid": "0030000000007801",
+                "area": "",
+                "building": "嘉风苑二期"
+            }
+        }
+
+    def ac(self, building: str, room: str) -> (str, float, float, float):
+        floor_id = ""
+        floor = ""
+
+        if self.cheat_sheet_pro[building]['head']:
+            floor_id = self.cheat_sheet_pro[building]['head'] + room[:2]
+            floor = room[1] + "层"
+        
+        data = {
+            "query_elec_roominfo": {
+                "aid": self.cheat_sheet_pro[building]['aid'],
+                "account": "000001",
+                "room": {
+                    "roomid": self.cheat_sheet_pro[building]['head'] + room,
+                    "room": room
+                },
+                "floor": {
+                    "floorid": floor_id,
+                    "floor": floor
+                },
+                "area": {
+                    "area": self.cheat_sheet_pro[building]['area'],
+                    "areaname": self.cheat_sheet_pro[building]['area']
+                },
+                "building": {
+                    "buildingid": self.cheat_sheet_pro[building]['building_id'],
+                    "building": self.cheat_sheet_pro[building]['building']
+                }
+            }
+        }
+
+        p1 = re.compile("\d+.房间剩余量(-?\d+.\d+)房间名称\(.*\)")
+        p2 = re.compile("账号id:i\d+-剩余金额:(-?\d+\.\d+)-剩余电补:(-?\d+\.\d+)-剩余水补:(-?\d+\.\d+)")
+        p3 = re.compile("账号余额:(-?\d+\.\d+)")
+
+        if self.cheat_sheet_pro[building]["aid"] == "0030000000002503":
+            data["query_elec_roominfo"]["room"]["roomid"] = self.cheat_sheet_pro[building]['head'] + room + "S"
+            data["query_elec_roominfo"]["room"]["room"] = room + "S"
+            res_S = requests.post(self.tsm, data={"jsondata": json.dumps(data), "funname": "synjones.onecard.query.elec.roominfo"})
+
+            data["query_elec_roominfo"]["room"]["roomid"] = self.cheat_sheet_pro[building]['head'] + room + "D"
+            data["query_elec_roominfo"]["room"]["room"] = room + "D"
+            res_D = requests.post(self.tsm, data={"jsondata": json.dumps(data), "funname": "synjones.onecard.query.elec.roominfo"})
+            p1_res_S = p1.findall(res_S.text)[0]
+            p1_res_D = p1.findall(res_D.text)[0]
+            print(p1_res_S, p1_res_D)
+            return f"电费剩余: {p1_res_D}, 水费剩余: {p1_res_S}", float(p1_res_D), float(p1_res_S), 0
+
+        res = requests.post(self.tsm, data={"jsondata": json.dumps(data), "funname": "synjones.onecard.query.elec.roominfo"})
+        try:
+            p2_res = p2.findall(res.text)[0]
+            return f"水电剩余: {p2_res[0]}, 电补剩余: {p2_res[1]}, 水补剩余: {p2_res[2]}", float(p2_res[0]), float(p2_res[1]), float(p2_res[2])
+        except:
+            p3_res = p3.findall(res.text)
+            return f"电费剩余: {p3_res[0]}", 0, 0, 0
+        
+
+if __name__ == "__main__":
+    ub = uBills()
+    print(ub.ac("朗风", "1001"))
+    print(ub.ac("和风B", "4070"))
+    print(ub.ac("嘉风B", "3014"))
